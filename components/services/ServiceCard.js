53d77db4390685b00047209a018a172ad8fda77c
Brandon <ihartappraisals@gmail.com>
2023-11-25 16:02:09 -0800

    fixes

--- components/services/ServiceCard.js
+++ components/services/ServiceCard.js
@@ -1,14 +1,18 @@
-import styles from './services.module.scss';
+import styles from "./services.module.scss";
 
-const ServiceCard = ({desc, icon}) => {
+const ServiceCard = ({ desc, icon }) => {
   return (
-    <div className={`${styles.serviceCard} flex flex-col items-center justify-center border-white-800 border-solid border-2 lg:p-4`}>
-    	<div className="h-[200px] w-[200px] flex items-center justify-center">{icon}</div>
-          <div className="max-w-[250px] border-white-800 border-solid border-t-2 lg:p-6">
-          <p className=" text-center text-base">{desc}</p>
-        </div>
+    <div
+      className={`${styles.serviceCard} my-8 flex flex-col items-center justify-center md:border-white-800 md:border-solid md:border-2 lg:p-4`}
+    >
+      <div className="h-[150px] w-[150px] flex items-center justify-center md:h-[200px] md:w-[200px]">
+        {icon}
+      </div>
+      <div className="max-w-[250px] md:border-white-800 md:border-solid md:border-t-2 lg:p-6">
+        <p className=" text-center text-base">{desc}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ServiceCard;
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,29 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 import Head from "next/head";
-import Footer from '../components/Footer';
-import Header from '../components/header/Header';
-import Landing from '../components/landing/Landing'
-import Services from '../components/services/Services'
-import Location from '../components/location/Location'
-import Artists from '../components/artists/Artists'
-import Filler from '../components/filler/Filler'
-import Visit from '../components/visit/Visit'
-const inter = Inter({ subsets: ['latin'] })
+import Footer from "../components/Footer";
+import Header from "../components/header/Header";
+import Landing from "../components/landing/Landing";
+import Services from "../components/services/Services";
+import Location from "../components/location/Location";
+import Artists from "../components/artists/Artists";
+import Filler from "../components/filler/Filler";
+import Visit from "../components/visit/Visit";
+import Document from "../components/document/Document";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-    <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400&display=swap" rel="stylesheet" />
-      </Head>
-    <main className={`min-h-screen  ${inter.className}`} >
-      <Header />
-      <Landing />
-      <Services />
-      <Filler />
-      <Artists />
-      <Visit />
-      <Location />
-      <Footer />
-    </main>
+      <main className={`min-h-screen  ${inter.className}`}>
+        <Header />
+        <Landing />
+        <Services />
+        <Filler />
+        <Artists />
+        <Visit />
+        <Location />
+        <Footer />
+      </main>
     </>
-  )
+  );
 }
